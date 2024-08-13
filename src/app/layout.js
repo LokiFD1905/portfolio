13b2f3583faf4a4d94e0afe516c1a3918a7457e6
hariharan7089 <hariharank007089@gmail.com>
2024-08-13 12:32:59 +0530

    init

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,6 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+      <meta name="google-site-verification" content="pX7f_2J9N1DSkcxG4E1Zzb3lezDqP61WYbTHBpgIhKE" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
